287372ab0d168a5b478bf1b8522bc610fed6ef27
BongaGougota0 <bonga_gougota@outlook.com>
2025-05-10 20:25:03 +0200

    rename auth folder. short description.

--- phumie_interface/src/app/app.routes.ts
+++ phumie_interface/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './authentication_components/login/login.component';
 import { SignupComponent } from './authentication_components/signup/signup.component';
 import { PhumieIndexComponent } from './phumie-index/phumie-index.component';
---
+++ phumie_interface/src/app/app_modals/post-modal/post-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-modal',
+  imports: [],
+  templateUrl: './post-modal.component.html',
+  styleUrl: '../authenticated_user/feed/feed.component.css'
+})
+export class PostModalComponent {
+
+}
