49886ac84c809360a1c07028384eda958abb80d8
BongaGougota0 <bonga_gougota@outlook.com>
2025-05-19 22:22:09 +0200

    create custom providers data to add HttpClient providers on main.ts

--- phumie_interface/src/main.ts
+++ phumie_interface/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
+const appCustomConfigs = {
+  ...appConfig,
+  providers: [
+    (appConfig.providers || []),
+    provideHttpClient()
+  ]
+}
+
+bootstrapApplication(AppComponent, appCustomConfigs)
   .catch((err) => console.error(err));
